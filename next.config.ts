43c74427e3ee66ebe23c4f193c2cd96a3a5cdaec
tenchan000517 <tenchan@nft-mint.xyz>
2026-05-03 02:07:02 +0900

    fix: correct industry image URLs to assets.yumesuta.com + register CDN remotePattern
    bucho 重大ミス修正:
    - 前 commit (b30c2e6) で設定した URL `https://yumesuta.com/img/career-guide/{slug}.png` は production HTTP 404
    - 正しい URL = `https://assets.yumesuta.com/career-guide/{slug}.png` (= yumesutaHP 業界 page で使用中の CDN URL・全 6 業界 HTTP 200 確認済)
    
    修正内容:
    - lib/career-industry-mapping.ts: 全 6 業界 image URL を assets.yumesuta.com/career-guide/{slug}.png に訂正
    - next.config.ts: images.remotePatterns に assets.yumesuta.com 1 件追加 (漆畑さん 5/3 例外承認)
    
    これで CareerExploreSection の業界カード画像表示が production で正常動作。

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'yumesuta.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'assets.yumesuta.com',
+      },
     ],
   },
 };
